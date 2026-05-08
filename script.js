60785b75ba842f80c0b862a52f1179d8083f4f5b
Oksana Budko <budkoksana@gmail.com>
2026-05-08 17:29:08 +0300

    add task from lesson01

---
+++ script.js
@@ -0,0 +1,10 @@
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+alert("the first message with alert");
+console.log("the second message with console");
+
