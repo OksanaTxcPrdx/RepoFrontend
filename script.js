474a4263370a6e484a36170e89b6621eb7cc7dff
Oksana Budko <budkoksana@gmail.com>
2026-05-10 21:22:58 +0300

    add tasks from lesson02 with data types

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "RepoFrontend";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 234;
+let rollback = 20;
+let fullPrice = 23000;
+let adaptive = true;
 
-alert("the first message with alert");
-console.log("the second message with console");
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
 
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов" + screenPrice + "долларов");
+console.log("Стоимость расработки сайта" + fullPrice + "долларов");
+
+console.log(screens.toLowerCase().split(", "));
+
+console.log(`Процент отката посреднику за работу ${fullPrice * (rollback / 100)}`)
